687e328367b73a2e865437cf4eb53b46b666c569
panchalleena1286 <panchalleena8839@gmail.com>
2023-02-24 12:28:51 +0530

    initial commit

--- socket-chat/src/component/ChatRoom.jsx
+++ socket-chat/src/component/ChatRoom.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const ChatRoom = () => {
+
+    const location = useLocation()
+
+    const [data, setdata] = useState({})
+    const [msg, setMsg] = useState("")
+    const [allMessages, setMessages] = useState([])
+
+    useEffect(() => {
+        setdata(location.state)
+    }, [location])
+
+    const handleChange = (e) => setMsg(e.target.value)
+
+    const onSubmit = () => {
+        const newMessage = {time: new Date(), msg, name: data.name}
+        setMessages([...allMessages, newMessage])
+    }
+
+    
     return (
         <div className="py-4 m-5 w-50 shadow bg-white text-dark border rounded container" style={{height: "100%"}}>
             <div className="text-center px-3 mb-4 text-capitalize">
-                <h1 className="text-info mb-4">Gaming Chat Box</h1>
+                <h1 className="text-info mb-4">{data?.room} Chat Box</h1>
             </div>
             <div className="bg-light border rounded p-3 mb-4" style={{height:"450px", overflowY:"scroll"}}>
                 <div className="row justify-content-end pl-5">
-                    <div className="d-flex flex-column align-items-end m-2 shadow p-2  border rounded w-auto" style={{background:"#E1F0F0"}}>
+                    {
+                        allMessages.map
+                    }
+                    <div className="d-flex flex-column align-items-end m-2 shadow p-2  border rounded w-auto" style={{background: "#DFE9E8"}} >
                         <div>
                             <strong className="m-1">Leena</strong>
                             <small className="text-muted">2 min ago</small>
@@ -26,8 +51,12 @@ const ChatRoom = () => {
                 </div>
             </div>
             <div className="form-group d-flex">
-                <input type="text" className="form-control bg-light" name="message" placeholder="Type your message"/>
-                <button type="button" className="btn btn-info mx-2 ">Send</button>
+                <input type="text" className="form-control bg-light" name="message" onChange={handleChange} value="msg" placeholder="Type your message"/>
+                <button type="button" className="btn btn-info mx-2" onClick={onSubmit}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+                </svg>
+                </button>
             </div>
         </div>
     )
